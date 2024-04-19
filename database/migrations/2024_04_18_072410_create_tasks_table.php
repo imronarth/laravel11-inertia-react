e6b9de3eada48b034043e8c4abaa7a5c11ccc5dc
imronarth <imron@gmx.com>
2024-04-19 14:22:25 +0700

    finish te migrate, database seed, and factory

--- database/migrations/2024_04_18_072410_create_tasks_table.php
+++ database/migrations/2024_04_18_072410_create_tasks_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('image_path')->nullable();
             $table->string('status');
             $table->string('priority');
+            $table->timestamp('due_date')->nullable();
             $table->foreignId('assigned_user_id')->constrained('users');
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('updated_by')->constrained('users');
